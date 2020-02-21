d8fd5076e66cd3b37a14c440c8be8619b1802448
rishik911 <rktatu610@gmail.com>
2020-02-21 07:28:24 +0000

    Local state added and api call working

--- AppModules/View/01_TakeawayListView.js
+++ AppModules/View/01_TakeawayListView.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 import SearchBar from "../Search/SearchBar";
+import {APIcall} from "../Network/ApiCall";
 
 class TakeawayListView extends Component {
+  componentDidMount(): void {
+    APIcall.takeAwayList()
+  }
+
   render() {
     return <SearchBar />;
   }
