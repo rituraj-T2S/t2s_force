5b3e9808eb5d81062e7f997d3c5611700dd7420a
rishik911 <rktatu610@gmail.com>
2020-02-21 08:17:19 +0000

    Added redux folders

---
+++ AppModules/Redux/TakeawayListSaga/TakeawayListSaga.js
@@ -0,0 +1,21 @@
+import {TakeAwayListTypes} from './TakeAwayListTypes';
+import { call, put, takeLatest, all } from "redux-saga/effects";
+import {APIcall} from '../Network/APIcall';
+
+export function * TakeAwayList_APIcall(action) {
+    try {
+        const postcode=yield call(APIcall.takeAwayList, action.payload)
+        yield put({type:TakeAwayListTypes.TAKEAWAYLIST_API_SUCCESS,postcode})
+    }
+    catch (error) {
+        yield put({type:TakeAwayListTypes.TAKEAWAYLIST_API_FAIL, error:action.payload})
+    }
+}
+export function * WatchTakeAwayList_APIcall() {
+    yield takeLatest(TakeAwayListTypes.TAKEAWAYLIST_API_REQUEST,TakeAwayList_APIcall)
+}
+
+export default function* RootSaga()
+{
+    yield all([WatchTakeAwayList_APIcall()])
+}
